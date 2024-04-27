cbf38c7f6c622d255ea3e2f0733aad964d7fdba1
KashanAdnan <fullstackdevkashan@gmail.com>
2024-04-27 16:00:42 +0500

    first commit

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <nav className="flex items-center justify-between px-24 py-4">
+            <p className="text-2xl">Kelexa.io</p>
+            <a href="/contact" className="">Contact Us</a>
+        </nav>
+    )
+}
+
+export default Navbar
